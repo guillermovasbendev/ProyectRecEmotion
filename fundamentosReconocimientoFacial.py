3d256d6937709b3e0d28f2c82ab7d4582dbf7c2f
guillermovasbendev <guillermovasbendev@gmail.com>
2023-10-01 11:26:05 -0500

    send changes in threshol adaptative, sen new archives threshol adaptative Gaussian, gradian Laplace and soluble, also changes inside images/creditos an images add harr_face.xml for recognized of faces an one file about how download the cascade for recognized

---
+++ fundamentosReconocimientoFacial.py
@@ -0,0 +1,10 @@
+# En el repositorio de gitHUb aparecen las cascadas para varias funciones, como reconocimiento de rostros
+
+# a continuacion la ruta: https://github.com/opencv/opencv/tree/master/data/haarcascades
+
+# para el curso usamos: haarcascade_frontalface_default.xml ubicado en la siguiente ruta:
+# https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+
+# hacer click en raw
+# copiar todo el codigo
+# traerlo a mi proyecto y pegarlo en un nuevo archivo, en este caso lo guardamos como haar_face.xml
--- umbralAdaptativoGaussiano.py
+++ umbralAdaptativoGaussiano.py
@@ -9,7 +9,7 @@ gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("gray",gray)
 
 # Umbral adaptativo 
-adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 2)
 cv.imshow("Umbral Adaptativo", adaptive_thresh)
 # gray: Este es el primer argumento y representa la imagen en escala de grises en la que se aplicará la umbralización.
 
@@ -19,7 +19,7 @@ cv.imshow("Umbral Adaptativo", adaptive_thresh)
 
 # v.adaptiveThreshold() : Se usa para establecer de forma automatica el punto de referencia
 
-# cv.ADAPTIVE_THRESH_MEAN_C: Este es el tercer parámetro y especifica el tipo de umbralización adaptativa que se utilizará. cv.ADAPTIVE_THRESH_MEAN_C indica que se utilizará el método de umbralización adaptativa basado en la media. En este método, el valor umbral se calcula como la media de los valores de intensidad en un área local alrededor de cada píxel. Esto significa que el umbral puede variar según las características locales de la imagen.
+# Cuando se utiliza cv.THRESH_GAUSSIAN_C, en lugar de calcular el umbral local basado en la media de los valores de intensidad en una ventana alrededor de cada píxel (como en cv.ADAPTIVE_THRESH_MEAN_C), se calcula utilizando la media ponderada de los valores de intensidad en la vecindad de cada píxel. El uso de la media ponderada, donde los valores más cercanos al píxel en cuestión tienen un peso mayor, permite un umbral local más suave y adaptado a las características locales de la imagen.
 
 # El "11" representa el tamaño de la vecindad (ventana) utilizada para calcular el umbral local. Un valor mayor considera una vecindad más grande, lo que puede suavizar el umbral y hacerlo más robusto frente a ruido en la imagen.
 
